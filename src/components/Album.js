663c2fadee52e91c9cc8c578abfac28b8eb7ee83
Sean Ryan <seanpryan89@gmail.com>
2019-01-05 13:57:51 -0500

    completed checkpoint-9-inputs

--- src/components/Album.js
+++ src/components/Album.js
@@ -14,11 +14,32 @@ class Album extends Component {
       album: album,
       currentSong: album.songs[0],
       isPlaying: false,
-      hover: false
+      hover: false,
+      currentTime: 0,
+      duration: album.songs[0].duration
     };
 
     this.audioElement = document.createElement("audio");
     this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  componentDidMount() {
+    this.eventListeners = {
+      timeupdate: e => {
+        this.setState({ currentTime: this.audioElement.currentTime });
+      },
+      durationchange: e => {
+        this.setState({ duration: this.audioElement.duration });
+      }
+    };
+    this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount() {
+    this.audioElement.src = null;
+    this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
   }
 
   play() {
@@ -68,6 +89,12 @@ class Album extends Component {
       this.setSong(newSong);
       this.play();
   }
+
+  handleTimeChange(e) {
+  const newTime = this.audioElement.duration * e.target.value;
+  this.audioElement.currentTime = newTime;
+  this.setState({ currentTime: newTime });
+}
 
   handleMouseEnter(song) {
     this.setState({ hover: song });
@@ -125,9 +152,12 @@ class Album extends Component {
           <PlayerBar
            isPlaying={this.state.isPlaying}
            currentSong={this.state.currentSong}
+           currentTime={this.audioElement.currentTime}
+           duration={this.audioElement.duration}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
            handleNextClick={() => this.handleNextClick()}
+           handleTimeChange={(e) => this.handleTimeChange(e)}
          />
       </section>
     );
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,29 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class PlayerBar extends Component {
   render() {
     return (
       <section className="player-bar">
-       <section id="buttons">
+        <section id="buttons">
           <button id="previous">
-            <span className="icon ion-md-skip-backward" onClick={this.props.handlePrevClick}></span>
+            <span
+              className="icon ion-md-skip-backward"
+              onClick={this.props.handlePrevClick}
+            />
           </button>
-          <button id="play-pause" onClick={this.props.handleSongClick} >
-            <span className={this.props.isPlaying ? 'icon ion-md-pause' : 'icon ion-md-play'}></span>
+          <button id="play-pause" onClick={this.props.handleSongClick}>
+            <span
+              className={
+                this.props.isPlaying ? "icon ion-md-pause" : "icon ion-md-play"
+              }
+            />
           </button>
           <button id="next" onClick={this.props.handleNextClick}>
-            <span className="icon ion-md-skip-forward"></span>
+            <span className="icon ion-md-skip-forward" />
           </button>
         </section>
         <section id="time-control">
-          <div className="current-time">–:––</div>
-          <input type="range" className="seek-bar" value="0" />
-          <div className="total-time">–:––</div>
+          <div className="current-time">{this.props.currentTime}</div>
+          <input
+            type="range"
+            className="seek-bar"
+            value={this.props.currentTime / this.props.duration || 0}
+            max="1"
+            min="0"
+            step="0.01"
+            onChange={this.props.handleTimeChange}
+          />
+          <div className="total-time">{this.props.duration}</div>
         </section>
         <section id="volume-control">
-          <div className="icon ion-volume-low"></div>
+          <div className="icon ion-volume-low" />
           <input type="range" className="seek-bar" value="80" />
-          <div className="icon ion-volume-high"></div>
+          <div className="icon ion-volume-high" />
         </section>
       </section>
     );
