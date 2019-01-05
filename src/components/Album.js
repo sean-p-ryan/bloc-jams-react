eed887f0c97e5c72c954c52c0d48ba1d2ad2abdd
Sean Ryan <seanpryan89@gmail.com>
2019-01-05 11:21:45 -0500

    completed assignment-8-buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -56,6 +56,19 @@ class Album extends Component {
       this.play();
   }
 
+  handleNextClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex + 1);
+      const newSong = this.state.album.songs[newIndex];
+      if (newIndex > this.state.album.songs.length - 1) {
+          this.setSong(this.state.album.songs[0]);
+          this.play();
+          return;
+        }
+      this.setSong(newSong);
+      this.play();
+  }
+
   handleMouseEnter(song) {
     this.setState({ hover: song });
   }
@@ -114,6 +127,7 @@ class Album extends Component {
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
          />
       </section>
     );
