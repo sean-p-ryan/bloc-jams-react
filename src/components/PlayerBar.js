662af48bee55d1d2098940d67f08cade76580ad3
Sean Ryan <seanpryan89@gmail.com>
2019-01-06 13:55:53 -0500

    got formatTime to work on different time displays

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -23,7 +23,7 @@ class PlayerBar extends Component {
           </button>
         </section>
         <section id="time-control">
-          <div className="current-time">{this.props.displayTime}</div>
+          <div className="current-time">{this.props.formatTime(this.props.currentTime)}</div>
           <input
             type="range"
             className="seek-bar"
@@ -33,7 +33,7 @@ class PlayerBar extends Component {
             step="0.01"
             onChange={this.props.handleTimeChange}
           />
-          <div className="total-time">{this.props.displayDuration}</div>
+          <div className="total-time">{this.props.formatTime(this.props.duration)}</div>
         </section>
         <section id="volume-control">
         <div className="current-volume">{this.props.currentVolume}</div>
