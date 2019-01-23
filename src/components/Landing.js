b32e199369c4cf6916298a7aa42096c2c54f75bf
Sean Ryan <seanpryan89@gmail.com>
2019-01-23 07:42:53 -0500

    included arrow icon in nav in link

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import React from "react";
+import { Route, Link } from "react-router-dom";
 import landing from "../styles/landing.css";
 
 const Landing = () => (
@@ -7,28 +7,30 @@ const Landing = () => (
     <header>
       <h1>Algo Rhythm</h1>
       <nav>
-        <Link className="Library" to='/library'>Library</Link>
-        <i className="icon ion-md-play" id="nav-arrow"></i>
+        <Link className="Library" to="/library">
+          Library<i className="icon ion-md-play" id="nav-arrow" />
+        </Link>
       </nav>
     </header>
     <section className="selling-points">
       <div className="dexter">
-       <p className="selling-point-text">Robot-powered jam selection and discovery.</p>
-       <div className="dexter-image">
-       </div>
+        <p className="selling-point-text">
+          Robot-powered jam selection and discovery.
+        </p>
+        <div className="dexter-image" />
       </div>
       <div className="beethoven">
-       <p className="selling-point-text">Rock your jams wherever you be, however you do.</p>
-       <div className="beethoven-image">
-       </div>
+        <p className="selling-point-text">
+          Rock your jams wherever you be, however you do.
+        </p>
+        <div className="beethoven-image" />
       </div>
       <div className="willie">
-       <p className="selling-point-text">You are a butterfly.</p>
-       <div className="willie-image">
-       </div>
+        <p className="selling-point-text">You are a butterfly.</p>
+        <div className="willie-image" />
       </div>
     </section>
   </body>
-)
+);
 
 export default Landing;
